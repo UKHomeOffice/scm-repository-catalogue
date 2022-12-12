9b1b1bba673bb958a44dea8832685bfb0ae831cc
Mahruf Iqbal <mahruf.iqbal@digital.homeoffice.gov.uk>
2022-12-12 09:48:12 +0000

    feat: Lists all GitHub Apps in an organization

---
+++ public/javascript/organisations-canvas.js
@@ -0,0 +1,41 @@
+
+var dataVar = await fetch('https://ukhomeoffice.github.io/scm-repository-catalogue/organisations.json')
+.then((response) => response.json())
+.then((data) => {
+  return data.organisationApps.values
+});
+
+new Chart(
+  document.getElementById('orgCanvas'),
+  {
+    type: 'line',
+    options: {
+      scales: {
+        x: {
+          // ticks: {
+          //   callback: value => 
+          //     `${new Date(dataVar[value].lastUpdatedAt).toLocaleDateString()}`
+          // },
+          type: 'time',
+          min: (new Date(new Date().getTime() - 12096e5)),
+          max: (new Date(new Date().getTime() + 8.64e7)),
+        },
+        y: {
+          min: 0,
+        }
+      }
+    },
+
+    data: {
+      labels: dataVar.map(row => row.lastUpdatedAt),
+      datasets: [
+        {
+          label: 'Total Installed Apps',
+          data: dataVar.map(row => row.total),
+          borderColor: '#0000FF',
+        },
+        
+      ]
+    }
+  }
+);
