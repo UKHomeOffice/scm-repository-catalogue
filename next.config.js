420d97749e9c02b296e91b83d4bfe824f8a2af1b
Ariful Haque <ariful.haque@digital.homeoffice.gov.uk>
2022-12-16 16:36:53 +0000

    Fix styling issue

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/scm-repository-catalogue'
+  basePath: '/scm-repository-catalogue',
+  assetPrefix: '/scm-repository-catalogue/',
 }
 
 module.exports = nextConfig
