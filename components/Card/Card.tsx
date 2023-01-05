bd938df634bdbe480c3f46754d32cea57a8bb613
samiwelthomasHO <113512609+samiwelthomasHO@users.noreply.github.com>
2023-01-05 16:30:47 +0000

    Add community profile score to repository view. (#89)

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,6 +2,7 @@ import styles from "./Card.module.css"
 import Image from "next/image";
 import starSvg from "./star.svg";
 import forkSvg from "./fork.svg";
+import communitySvg from "./community.svg";
 import Link from "next/link";
 
 interface RepoCardProps {
@@ -19,6 +20,9 @@ interface RepoCardProps {
     forksCount: number
     updatedAt: string
     pushedAt: string
+    communityProfile?: {
+      health_percentage: number
+    }
   }
 
 }
@@ -30,25 +34,30 @@ const Card: React.FC<RepoCardProps> = ({repo}) => (<div className={styles.card}>
 
   <div className={styles.container}>
 
-  <div className={styles.labels}>
-    {repo.language && (<div>{repo.language}</div>) }
+    <div className={styles.labels}>
+      {repo.language && (<div>{repo.language}</div>)}
 
-  <div>{repo.visibility}</div>
+      <div>{repo.visibility}</div>
 
-  {repo.license && (
-    <div>{repo.license.name}</div>
-  )}
-  </div>
-
-
-  <div className={styles.stats}>
-    <div>
-    <Image width={10} height={10} src={starSvg} alt={"Number of stars"} /> {repo.stargazersCount}
+      {repo.license && (
+        <div>{repo.license.name}</div>
+      )}
     </div>
-  <div>
-    <Image width={10} height={10} src={forkSvg} alt={"Number of forks"} />{repo.forksCount}
-  </div>
-  </div>
+
+
+    <div className={styles.stats}>
+      <div>
+        <Image width={10} height={10} src={starSvg} alt={"Number of stars"}/> {repo.stargazersCount}
+      </div>
+      <div>
+        <Image width={10} height={10} src={forkSvg} alt={"Number of forks"}/>{repo.forksCount}
+      </div>
+      <div>
+        <Image width={10} height={10} src={communitySvg}
+               alt={"Community profile score"}/>{`${repo.communityProfile?.health_percentage || 0}%`}
+      </div>
+
+    </div>
   </div>
 
 
