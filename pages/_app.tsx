e669b5062ccf5e4d6fac8d506ee1125a3c9113cc
samiwelthomasHO <113512609+samiwelthomasHO@users.noreply.github.com>
2023-01-10 14:50:25 +0000

    Fix missing tooltips. (#107)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../public/stylesheets/govuk-frontend-ie8-4.3.0.min.css";
 import "../public/stylesheets/govuk-frontend-4.3.0.min.css";
 import "../public/stylesheets/overrides.css";
+import "react-tooltip/dist/react-tooltip.css";
 
 import type { AppProps } from "next/app";
 
