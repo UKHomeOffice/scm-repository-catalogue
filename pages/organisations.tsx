2d17fec06b3e926363cc8a6110d520038cbd8ac6
samiwelthomasHO <113512609+samiwelthomasHO@users.noreply.github.com>
2023-01-10 16:10:03 +0000

    Fix active links (#108)

--- pages/organisations.tsx
+++ pages/organisations.tsx
@@ -1,7 +1,7 @@
 import organisationJson from "../public/organisations.json";
 import packagesJson from "../public/organisations-packages.json";
 import appColoursJson from "../public/apps-colour.json";
-import { groupBy, last, capitalize, repeat, isEmpty } from "lodash";
+import { groupBy, last, capitalize, isEmpty } from "lodash";
 
 import "chartjs-adapter-moment";
 import {
@@ -17,6 +17,7 @@ import { Bar } from "react-chartjs-2";
 import React from "react";
 import Card from "../components/Card";
 import GridLayout from "../components/GridLayout";
+import Header from "../components/Header";
 
 ChartJS.register(
   CategoryScale,
@@ -103,113 +104,116 @@ export default function Organisations({
 }: OrganisationPageProps) {
   return (
     <>
-      <div className="govuk-width-container ">
-        <h2 className="govuk-heading-l">Maturity</h2>
+      <Header />
+      <div className={"govuk-width-container"}>
+        <main className={"govuk-main-wrapper"} id="main-content" role="main">
+          <h2 className="govuk-heading-l">Maturity</h2>
 
-        <h3 className="govuk-heading-m">Current</h3>
-        <div className="maturity-stats-container">
-          <div className="maturity-stat maturity-level-2">
-            <div className="maturity-stat-value">46%</div>
-            <div className="maturity-stat-name">Multiple Owners</div>
+          <h3 className="govuk-heading-m">Current</h3>
+          <div className="maturity-stats-container">
+            <div className="maturity-stat maturity-level-2">
+              <div className="maturity-stat-value">46%</div>
+              <div className="maturity-stat-name">Multiple Owners</div>
+            </div>
+            <div className="maturity-stat maturity-level-2">
+              <div className="maturity-stat-value">37%</div>
+              <div className="maturity-stat-name">Use Teams</div>
+            </div>
+            <div className="maturity-stat maturity-level-4">
+              <div className="maturity-stat-value">70%</div>
+              <div className="maturity-stat-name">Open source packages</div>
+            </div>
           </div>
-          <div className="maturity-stat maturity-level-2">
-            <div className="maturity-stat-value">37%</div>
-            <div className="maturity-stat-name">Use Teams</div>
-          </div>
-          <div className="maturity-stat maturity-level-4">
-            <div className="maturity-stat-value">70%</div>
-            <div className="maturity-stat-name">Open source packages</div>
-          </div>
-        </div>
 
-        <h3 className="govuk-heading-m">Target</h3>
-        <div className="maturity-stats-container">
-          <div className="maturity-stat maturity-level-5">
-            <div className="maturity-stat-value">80%</div>
-            <div className="maturity-stat-name">Multiple Owners</div>
+          <h3 className="govuk-heading-m">Target</h3>
+          <div className="maturity-stats-container">
+            <div className="maturity-stat maturity-level-5">
+              <div className="maturity-stat-value">80%</div>
+              <div className="maturity-stat-name">Multiple Owners</div>
+            </div>
+            <div className="maturity-stat maturity-level-5">
+              <div className="maturity-stat-value">86%</div>
+              <div className="maturity-stat-name">Use Teams</div>
+            </div>
+            <div className="maturity-stat maturity-level-5">
+              <div className="maturity-stat-value">100%</div>
+              <div className="maturity-stat-name">Open source packages</div>
+            </div>
           </div>
-          <div className="maturity-stat maturity-level-5">
-            <div className="maturity-stat-value">86%</div>
-            <div className="maturity-stat-name">Use Teams</div>
-          </div>
-          <div className="maturity-stat maturity-level-5">
-            <div className="maturity-stat-value">100%</div>
-            <div className="maturity-stat-name">Open source packages</div>
-          </div>
-        </div>
 
-        <h1 className="govuk-heading-l">Organisations</h1>
+          <h1 className="govuk-heading-l">Organisations</h1>
 
-        <GridLayout cols={4}>
-          {orgs.map((org: string) => (
-            <Card
-              key={`${org}-pType`}
-              title={org}
-              titleLinkUrl={`https://github.com/${org}`}
-              indicators={PACKAGE_TYPES.map((pType) => ({
-                name: pType,
-                value: orgData[org][0].packages[pType]?.length || "0",
-                imageSrc: `/scm-repository-catalogue/assets/images/${pType}.png`,
-                tooltipLabel: capitalize(pType),
-              }))}
-            />
-          ))}
-        </GridLayout>
-        <h2 className="govuk-heading-l">Github Installed Apps</h2>
-
-        <GridLayout cols={2} style={{ maxWidth: "430px" }}>
-          {Object.keys(orgData).map((org: string) => {
-            // @ts-ignore
-            const appsData = groupBy(last(orgData[org]).installedApps, (yr) =>
-              new Date(yr.installedAt).getUTCFullYear()
-            );
-
-            return isEmpty(appsData) ? null : (
-              <Bar
-                style={{
-                  border: "1px solid #ccc",
-                }}
-                id={org}
-                key={org}
-                options={{
-                  plugins: {
-                    legend: {
-                      display: true,
-                      position: "bottom",
-                    },
-                    title: {
-                      display: true,
-                      text: orgData[org][0].organisation,
-                    },
-                  },
-                  responsive: true,
-                  scales: {
-                    x: {
-                      stacked: true,
-                    },
-                    y: {
-                      stacked: true,
-                      ticks: {
-                        precision: 0,
-                      },
-                    },
-                  },
-                }}
-                data={{
-                  datasets: Object.keys(appsData).flatMap((year) => {
-                    return appsData[year].map((app) => ({
-                      label: app.app,
-                      data: {
-                        [year]: 1,
-                      },
-                      backgroundColor: COLORS[app.app],
-                    }));
-                  }),
-                }}
+          <GridLayout cols={4}>
+            {orgs.map((org: string) => (
+              <Card
+                key={`${org}-pType`}
+                title={org}
+                titleLinkUrl={`https://github.com/${org}`}
+                indicators={PACKAGE_TYPES.map((pType) => ({
+                  name: pType,
+                  value: orgData[org][0].packages[pType]?.length || "0",
+                  imageSrc: `/scm-repository-catalogue/assets/images/${pType}.png`,
+                  tooltipLabel: capitalize(pType),
+                }))}
               />
-            );
-          })}
-        </GridLayout>
+            ))}
+          </GridLayout>
+          <h2 className="govuk-heading-l">Github Installed Apps</h2>
+
+          <GridLayout cols={2} style={{ maxWidth: "430px" }}>
+            {Object.keys(orgData).map((org: string) => {
+              // @ts-ignore
+              const appsData = groupBy(last(orgData[org]).installedApps, (yr) =>
+                new Date(yr.installedAt).getUTCFullYear()
+              );
+
+              return isEmpty(appsData) ? null : (
+                <Bar
+                  style={{
+                    border: "1px solid #ccc",
+                  }}
+                  id={org}
+                  key={org}
+                  options={{
+                    plugins: {
+                      legend: {
+                        display: true,
+                        position: "bottom",
+                      },
+                      title: {
+                        display: true,
+                        text: orgData[org][0].organisation,
+                      },
+                    },
+                    responsive: true,
+                    scales: {
+                      x: {
+                        stacked: true,
+                      },
+                      y: {
+                        stacked: true,
+                        ticks: {
+                          precision: 0,
+                        },
+                      },
+                    },
+                  }}
+                  data={{
+                    datasets: Object.keys(appsData).flatMap((year) => {
+                      return appsData[year].map((app) => ({
+                        label: app.app,
+                        data: {
+                          [year]: 1,
+                        },
+                        backgroundColor: COLORS[app.app],
+                      }));
+                    }),
+                  }}
+                />
+              );
+            })}
+          </GridLayout>
+        </main>
       </div>
     </>
   );
